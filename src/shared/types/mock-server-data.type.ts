176029517e1e19f39c812714c041eab0f3eb0843
Kseniia <kseniia.lazarov@zoner.com>
2025-03-07 16:48:02 +0100

    Add: type for MockServerData

---
+++ src/shared/types/mock-server-data.type.ts
@@ -0,0 +1,20 @@
+export type Location = {
+  longitude: number;
+  latitude: number;
+};
+
+export type MockServerData = {
+  offerNames: string[];
+  descriptions: string[];
+  cities: string[];
+  previewImages: string[];
+  images: string[];
+  roomTypes: string[];
+  services: string[];
+  users: string[];
+  emails: string[];
+  passwords: string[];
+  avtarPaths: string[];
+  userTypes: string[];
+  locaations: Location[];
+};
