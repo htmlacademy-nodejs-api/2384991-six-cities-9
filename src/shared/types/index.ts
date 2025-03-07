f9044e910321cf383a220d30c5a4bdd11c1cd898
Kseniia <kseniia.lazarov@zoner.com>
2025-03-07 16:52:48 +0100

    Add: interface OfferGenerator

--- src/shared/types/index.ts
+++ src/shared/types/index.ts
@@ -4,3 +4,4 @@ export { User } from './user.type.js';
 export { RoomType } from './room-type.enum.js';
 export { City } from './city.enum.js';
 export { Services } from './services.type.js';
+export { MockServerData } from './mock-server-data.type.js';
