846fbde42854dec9009406c7ad39a74f7ca1fb10
Kseniia <kseniia.lazarov@zoner.com>
2025-03-14 09:36:33 +0100

    Refactor: adds support of dotenv in RestConfig

--- src/main.rest.ts
+++ src/main.rest.ts
@@ -1,10 +1,12 @@
 import { PinoLogger } from './shared/libs/logger/index.js';
 import { RestApplication } from './rest/index.js';
+import { RestConfig } from './shared/libs/config/index.js';
 
 const bootstrap = async () => {
   const logger = new PinoLogger();
+  const config = new RestConfig(logger);
 
-  const restApplication = new RestApplication(logger);
+  const restApplication = new RestApplication(logger, config);
   await restApplication.init();
 };
 
