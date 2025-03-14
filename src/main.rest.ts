f2beb092c7bb7c015110b68e39233c4191efec1e
Kseniia <kseniia.lazarov@zoner.com>
2025-03-14 11:39:54 +0100

    Add: dependency container, decorator injectable from iversify,  implement depednecies

--- src/main.rest.ts
+++ src/main.rest.ts
@@ -1,12 +1,18 @@
-import { PinoLogger } from './shared/libs/logger/index.js';
+import 'reflect-metadata';
+import { Container } from 'inversify';
+import { Logger, PinoLogger } from './shared/libs/logger/index.js';
 import { RestApplication } from './rest/index.js';
-import { RestConfig } from './shared/libs/config/index.js';
+import { Config, RestConfig, RestSchema } from './shared/libs/config/index.js';
+import { Component } from './shared/types/component.enum.js';
 
 const bootstrap = async () => {
-  const logger = new PinoLogger();
-  const config = new RestConfig(logger);
+  const container = new Container();
 
-  const restApplication = new RestApplication(logger, config);
+  container.bind<Logger>(Component.Logger).to(PinoLogger).inSingletonScope();
+  container.bind<Config<RestSchema>>(Component.Config).to(RestConfig).inSingletonScope();
+  container.bind<RestApplication>(Component.RestApplication).to(RestApplication).inSingletonScope();
+
+  const restApplication = container.get<RestApplication>(Component.RestApplication);
   await restApplication.init();
 };
 
