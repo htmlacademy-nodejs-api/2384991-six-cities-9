7f0ac212b03dbc5f69a529b0565ab5f33ded63c4
Kseniia <kseniia.lazarov@zoner.com>
2025-03-14 11:18:29 +0100

    Add: constant object Component

--- src/shared/types/index.ts
+++ src/shared/types/index.ts
@@ -5,3 +5,4 @@ export { RoomType } from './room-type.enum.js';
 export { City } from './city.enum.js';
 export { Services } from './services.enum.js';
 export { MockServerData } from './mock-server-data.type.js';
+export { Component } from './component.enum.js';
