17639599d1e50e9a037c001b8162408b78a0a201
Kseniia <inanast153@gmail.com>
2025-03-16 18:42:50 +0100

    Add: upgrade RestSchema

--- src/shared/libs/config/rest.schema.ts
+++ src/shared/libs/config/rest.schema.ts
@@ -6,7 +6,11 @@ convict.addFormats(validator);
 export type RestSchema = {
   PORT: number;
   SALT: string;
-  DB_HOST: string
+  DB_HOST: string;
+  DB_USER: string;
+  DB_PASSWORD: string;
+  DB_PORT: string;
+  DB_NAME: string;
 }
 
 export const configRestSchema = convict<RestSchema>({
