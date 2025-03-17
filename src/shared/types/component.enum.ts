f861f382882b2b1d66c2cea5227924058d5df620
Kseniia <kseniia.lazarov@zoner.com>
2025-03-17 08:50:39 +0100

    Add: function-helper getMongoURI, new component to Component

--- src/shared/types/component.enum.ts
+++ src/shared/types/component.enum.ts
@@ -2,4 +2,5 @@ export const Component = {
   RestApplication: Symbol('RestApplication'),
   Logger: Symbol('Logger'),
   Config: Symbol('Config'),
+  DatabaseClient: Symbol('DatabaseClient'),
 } as const;
