ba0329c7e71c007234a7da70794f4494b9da8546
Kseniia <kseniia.lazarov@zoner.com>
2025-03-17 09:01:46 +0100

    Add: connection to Database

--- src/main.rest.ts
+++ src/main.rest.ts
@@ -4,6 +4,7 @@ import { Logger, PinoLogger } from './shared/libs/logger/index.js';
 import { RestApplication } from './rest/index.js';
 import { Config, RestConfig, RestSchema } from './shared/libs/config/index.js';
 import { Component } from './shared/types/component.enum.js';
+import { DatabaseClient, MongoDatabaseClient } from './shared/libs/database-client/index.js';
 
 const bootstrap = async () => {
   const container = new Container();
@@ -11,6 +12,7 @@ const bootstrap = async () => {
   container.bind<Logger>(Component.Logger).to(PinoLogger).inSingletonScope();
   container.bind<Config<RestSchema>>(Component.Config).to(RestConfig).inSingletonScope();
   container.bind<RestApplication>(Component.RestApplication).to(RestApplication).inSingletonScope();
+  container.bind<DatabaseClient>(Component.DatabaseClient).to(MongoDatabaseClient).inSingletonScope();
 
   const restApplication = container.get<RestApplication>(Component.RestApplication);
   await restApplication.init();
