c7375c32326dae5097f1fd2d9c860371913b99e5
Kseniia <kseniia.lazarov@zoner.com>
2025-03-17 09:55:28 +0100

    Add: repeated connection to DB

--- src/shared/libs/database-client/mongo.database-client.ts
+++ src/shared/libs/database-client/mongo.database-client.ts
@@ -1,8 +1,12 @@
 import * as Mongoose from 'mongoose';
 import { inject, injectable } from 'inversify';
 import { DatabaseClient } from './database-client.interface.js';
+import { setTimeout } from 'node:timers/promises';
 import { Component } from '../../types/index.js';
 import { Logger } from '../logger/index.js';
+
+const RETRY_COUNT = 5;
+const RETRY_TIMEOUT = 1000;
 
 @injectable()
 export class MongoDatabaseClient implements DatabaseClient {
