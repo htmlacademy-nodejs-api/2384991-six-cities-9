8e07dd91bd1dd9689239f2bb1bbac3ff57e201aa
Kseniia <kseniia.lazarov@zoner.com>
2025-03-24 10:24:30 +0100

    Add: install nodemon, update interface OfferService (add findOfferList)

--- src/shared/modules/offer/offer-service.interface.ts
+++ src/shared/modules/offer/offer-service.interface.ts
@@ -4,12 +4,13 @@ import { OfferEntity, CreateOfferDto } from './index.js';
 export interface OfferService {
   create(dto: CreateOfferDto): Promise<DocumentType<OfferEntity>>;
   findById(offerId: string): Promise<DocumentType<OfferEntity> | null>;
+  findOfferList(): Promise<DocumentType<OfferEntity>[]>;
 
 /* prepared for using in the next commits
 
 update(offerId: string, dto: CreateOfferDto): Promise<DocumentType<OfferEntity> | null>;
   delete(offerId: string): Promise<DocumentType<OfferEntity> | null>;
-  findOfferList(): Promise<DocumentType<OfferEntity>[]>;
+ 
   findPremiumByCity(city: string): Promise<DocumentType<OfferEntity>[]>;
   findFavorites(userId: string): Promise<DocumentType<OfferEntity>[]>;
   addFavorite(userId: string, offerId: string): Promise<void>;
