b1ee1741d356ede058314a241a1ccc49b9e715e2
Kseniia <kseniia.lazarov@zoner.com>
2025-03-26 12:33:23 +0100

    Add: an response  object (RDO) with class-transformer package using

--- src/shared/helpers/common.ts
+++ src/shared/helpers/common.ts
@@ -1,3 +1,5 @@
+import { ClassConstructor, plainToInstance } from "class-transformer";
+
 export const generateRandomValue = (min:number, max: number, numAfterDigit = 0) => +((Math.random() * (max - min)) + min).toFixed(numAfterDigit);
 
 export const getRandomItems = <T>(items: T[]): T[] => {
@@ -14,3 +16,5 @@ export const getExactlyNItems = <T>(items: T[], count: number): T[] => {
 export const getRandomItem = <T>(items: T[]): T => items[generateRandomValue(0, items.length - 1)];
 
 export const getErrorMessage = (error: unknown): string => error instanceof Error ? error.message : '';
+
+export const fillDTO = <T, V>(someDto: ClassConstructor<T>, plainObject: V) => plainToInstance(someDto, plainObject, { excludeExtraneousValues: true });
--- src/shared/modules/offer/index.ts
+++ src/shared/modules/offer/index.ts
@@ -4,3 +4,4 @@ export { OfferService } from './offer-service.interface.js';
 export { DefaultOfferService } from './default-offer.service.js';
 export { createOfferContainer } from './offer.container.js';
 export { OfferController } from './offer.controller.js';
+export { OfferRdo } from './rdo/offer.rdo.js';
